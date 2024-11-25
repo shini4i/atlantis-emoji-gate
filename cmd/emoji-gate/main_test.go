90bc8e1029093ab37626a16f0d3558b16b922703
Vadim Gedz <vadims@linux-tech.io>
2024-11-25 22:21:33 +0200

    chore: increase test coverage

--- cmd/emoji-gate/main_test.go
+++ cmd/emoji-gate/main_test.go
@@ -167,3 +167,56 @@ func TestRun(t *testing.T) {
 		assert.Equalf(t, 1, exitCode, "Expected exit code 1, got %d", exitCode)
 	})
 }
+
+// TestCanApprove tests the CanApprove function.
+func TestCanApprove(t *testing.T) {
+	cfg := GitlabConfig{
+		ApproveEmoji:  "thumbsup",
+		MrAuthor:      "author",
+		Insecure:      false,
+		TerraformPath: ".",
+	}
+
+	codeOwnersProcessor := CodeOwnersProcessor{}
+
+	t.Run("Owner does not match", func(t *testing.T) {
+		owner := CodeOwner{Owner: "user2", Path: "*"}
+		reaction := &AwardEmoji{Name: "thumbsup", User: struct {
+			Username string `json:"username"`
+		}{Username: "user1"}}
+
+		canApprove := codeOwnersProcessor.CanApprove(owner, reaction, cfg)
+		assert.False(t, canApprove)
+	})
+
+	t.Run("Emoji does not match", func(t *testing.T) {
+		owner := CodeOwner{Owner: "user1", Path: "*"}
+		reaction := &AwardEmoji{Name: "thumbsdown", User: struct {
+			Username string `json:"username"`
+		}{Username: "user1"}}
+
+		canApprove := codeOwnersProcessor.CanApprove(owner, reaction, cfg)
+		assert.False(t, canApprove)
+	})
+
+	t.Run("MR author cannot approve their own MR", func(t *testing.T) {
+		owner := CodeOwner{Owner: "author", Path: "*"}
+		reaction := &AwardEmoji{Name: "thumbsup", User: struct {
+			Username string `json:"username"`
+		}{Username: "author"}}
+
+		canApprove := codeOwnersProcessor.CanApprove(owner, reaction, cfg)
+		assert.False(t, canApprove)
+	})
+
+	t.Run("Path does not match", func(t *testing.T) {
+		owner := CodeOwner{Owner: "user1", Path: "non-matching-path"}
+		reaction := &AwardEmoji{Name: "thumbsup", User: struct {
+			Username string `json:"username"`
+		}{Username: "user1"}}
+
+		cfg.TerraformPath = "different-path"
+		canApprove := codeOwnersProcessor.CanApprove(owner, reaction, cfg)
+		assert.False(t, canApprove)
+	})
+}
