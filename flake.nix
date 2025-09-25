d52f5f3a45d715eeda1091c082de311c18c9b602
Vadim Gedz <github@shini4i.dev>
2025-09-25 21:26:43 +0300

    chore: add nix setup (#22)
    * chore: add nix dev setup
    
    * chore: add delve to flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A development environment for the atlantis-emoji-gate Go project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      goToolchain = pkgs.go_1_24;
+
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          goToolchain
+          pkgs.gopls
+          pkgs.delve
+          pkgs.golangci-lint
+          pkgs.gitleaks
+          pkgs.go-junit-report
+        ];
+      };
+
+      packages.${system}.default = pkgs.buildGoModule {
+        pname = "atlantis-emoji-gate";
+        version = "0.4.0";
+        src = ./.;
+        vendorHash = "sha256-OVEY4TPnZ2Sq4wc8Zg6zhnRms8Jm1cfZK1EzzOWUasM=";
+      };
+    };
+}
